5aa6f355ad9a9f8ff8c6eee11c2e61cbd3490acd
YasukoItoi <56959040+YasukoItoi@users.noreply.github.com>
2019-12-19 08:27:15 +0900

    top/about/outline/works/popu.jsを更新しました

---
+++ popup.js
@@ -0,0 +1,7 @@
+$("#open_btn").on('click', function(){
+  $(".popup-overlay, popup-content").addClass("active");
+});
+
+$("#close_btn").on("click",function(){
+  $("popup-overlay, .popup-content").removeclass("active");
+});
