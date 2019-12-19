0478d7afd8f153f608f6083ed6410073ac67b7cb
YasukoItoi <56959040+YasukoItoi@users.noreply.github.com>
2019-12-19 18:09:15 +0900

    popupをちょっと変えたけどできません

--- popup.js
+++ popup.js
@@ -1,7 +1,15 @@
-$(".btn-img").on('click', function(){
-  $(".popup-overlay, .popup-content").addClass("active");
+//$('btn').on('click', function(){
+//  $(".popup-overlay, .popup-content").addClass("active");
+//});
+
+//$(".close_btn").on("click",function(){
+//  $(".popup-overlay, .popup-content").removeclass("active");
+//});//
+
+$('btn').on('click',function(){
+    $('.popup-overlay, .popup-content').addClass('active').fadeIn();
 });
 
-$(".close_btn").on("click",function(){
-  $(".popup-overlay, .popup-content").removeclass("active");
+$('#close_btn').on('click',function(){
+    $('.popup-overlay, popup-content').removeClass('active').fadeOut();
 });
