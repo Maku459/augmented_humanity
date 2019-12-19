8fe17264f810b841c20befe8a3c0f7851c43d705
YasukoItoi <56959040+YasukoItoi@users.noreply.github.com>
2019-12-19 13:55:44 +0900

    jquery読み込みました

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
-$("#open_btn").on('click', function(){
+$(".open_btn").on('click', function(){
   $(".popup-overlay, popup-content").addClass("active");
 });
 
-$("#close_btn").on("click",function(){
+$(".close_btn").on("click",function(){
   $("popup-overlay, .popup-content").removeclass("active");
 });
